269bcc0620456019322bc8b99534ee222ad94e44
autoAu <151525579+autoAu@users.noreply.github.com>
2026-05-08 23:25:37 +0800

    1

--- character/character.gd
+++ character/character.gd
@@ -2,14 +2,29 @@ extends CharacterBody2D
 
 @export var health : int
 @export var damage : int
+@export var jump_intensity: float
 @export var speed : float
 
 @onready var animation_player := $AnimationPlayer
 @onready var character_sprite := $CharacterSprite
 @onready var damage_emitter := $DamageEmitter
 
-enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, LAND}
+const GRAVITY := 600
 
+enum State {IDLE, WALK, ATTACK, TAKEOFF, JUMP, LAND, JUMPKICK}
+
+var anim_map :={
+	State.IDLE: "idle",
+	State.WALK: "walk",
+	State.ATTACK: "punch",
+	State.TAKEOFF: "takeoff",
+	State.JUMP: "jump",
+	State.LAND: "land",
+	State.JUMPKICK: "jumpkick",
+}
+
+var height: float
+var height_speed: float
 var state = State.IDLE
 
 func _ready() -> void:
@@ -19,7 +34,9 @@ func _process(delta: float) -> void:
 	flip_sprite()
 	handle_movement()
 	handle_animations()
+	handle_air_time(delta)
 	handle_input()
+	character_sprite.position = Vector2.UP * height
 	move_and_slide()
 
 func handle_movement() -> void:
@@ -34,14 +51,23 @@ func handle_input() -> void:
 	velocity = direction * speed
 	if can_attack() and Input.is_action_just_pressed("attack"):
 		state = State.ATTACK
+	if can_jump() and Input.is_action_just_pressed("jump"):
+		state = State.TAKEOFF
+	if can_jumpkick() and Input.is_action_just_pressed("attack"):
+		state = State.JUMPKICK
 
 func handle_animations() -> void:
-	if state == State.IDLE:
-		animation_player.play("idle")
-	elif state == State.WALK:
-		animation_player.play("walk")
-	elif state == State.ATTACK:
-		animation_player.play("punch")
+	if animation_player.has_animation(anim_map[state]):
+		animation_player.play(anim_map[state])
+		
+func handle_air_time(delta: float) -> void:
+	if state == State.JUMP or state == State.JUMPKICK:
+		height += height_speed * delta
+		if height < 0:
+			height = 0
+			state = State.LAND
+		else:
+			height_speed -= GRAVITY * delta
 		
 func flip_sprite() -> void:
 	if velocity.x > 0:
@@ -57,11 +83,18 @@ func can_move() -> bool:
 func can_attack() -> bool:
 	return state == State.IDLE or state == State.WALK
 
+func can_jump() -> bool:
+	return state == State.IDLE or state == State.WALK
+
+func can_jumpkick() -> bool:
+	return state == State.JUMP
+
 func on_action_complete() -> void:
 	state = State.IDLE
 
 func on_takeoff_complete() -> void:
 	state = State.JUMP
+	height_speed = jump_intensity
 
 func on_land_complete() -> void:
 	state = State.IDLE
@@ -69,4 +102,3 @@ func on_land_complete() -> void:
 func on_emit_damage(damage_receiver: DamageReceiver) -> void:
 	var direction := Vector2.LEFT if damage_receiver.global_position.x < global_position.x else Vector2.RIGHT
 	damage_receiver.damage_received.emit(damage, direction)
-	
